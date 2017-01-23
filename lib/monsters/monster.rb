38f4deafb32cf6734fb50a61ca128ed636a2812d
Edouard Pelosi <edouard.pelosi@gmail.com>
2017-01-23 10:14:25 +0900

    monsters instance only keep status. Rules are going to be applied elsewhere

--- lib/monsters/monster.rb
+++ lib/monsters/monster.rb
@@ -8,27 +8,10 @@ module Monsters
 
     def apply(**actions)
       health += actions[:heal]
+      health -= actions[:damage]
       experience += actions[:xp] || actions[:experience]
       spells -= actions[:use_spells]
       ammo -= actions[:use_ammo]
-
-      actions[:damages].each do |damage|
-        dp = damage_modification(damage)
-        dp = damage_saves(damage.type, dp)
-      end
-    end
-
-    private
-
-    def damage_modification(points)
-      return 0 if immune?(damage.type)
-      if weak?(damage.type)
-      elsif resistant?(damage.type)
-        # TODO: check actual modifications
-        damage.points / 2
-      else
-        damage.points
-      end
     end
   end
 end
