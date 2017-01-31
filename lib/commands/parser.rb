d5c480233ff737328154d9e89618711e9833797f
Edouard Pelosi <edouard.pelosi@gmail.com>
2017-01-31 20:12:38 +0900

    typos

--- lib/commands/parser.rb
+++ lib/commands/parser.rb
@@ -4,8 +4,8 @@ require 'actions/system'
 module Commands
   class Parser
     ACCEPTED_OPERATORS = ['=', ':', ':=']
-    ASSIGNEMENTS_PATTERN = /\s*(#{ACCEPTED_OPERATORS.join('|')})\s*/
-    BASIC_ASSIGNEMENT_OPERATOR = ':'
+    ASSIGNMENTS_PATTERN = /\s*(#{ACCEPTED_OPERATORS.join('|')})\s*/
+    BASIC_ASSIGNMENT_OPERATOR = ':'
 
     def initialize
       _prepare_commands
@@ -13,7 +13,7 @@ module Commands
     end
 
     def parse(command)
-      string = _clean_assignements(command).downcase
+      string = _clean_assignments(command).downcase
       result = @regexp.match(string)
       if result
         command_name, param_string = result.captures
@@ -43,16 +43,16 @@ module Commands
       end
     end
 
-    def _clean_assignements(string)
-      string.gsub(ASSIGNEMENTS_PATTERN, BASIC_ASSIGNEMENT_OPERATOR)
+    def _clean_assignments(string)
+      string.gsub(ASSIGNMENTS_PATTERN, BASIC_ASSIGNMENT_OPERATOR)
     end
 
     def _parse_params(param_string)
       return [[], {}] unless param_string && !param_string.empty?
       bits = param_string.split(/\s+/)
-      mandatory = bits.take_while { |s| (/#{BASIC_ASSIGNEMENT_OPERATOR}/ =~ s).nil? } || []
+      mandatory = bits.take_while { |s| (/#{BASIC_ASSIGNMENT_OPERATOR}/ =~ s).nil? } || []
       options = bits[mandatory.size..-1].map do |s|
-        h, *t = s.split(BASIC_ASSIGNEMENT_OPERATOR)
+        h, *t = s.split(BASIC_ASSIGNMENT_OPERATOR)
         [h.to_sym, (t && !t.empty?) ? t.join('_') : true]
       end.to_h || {}
       [mandatory, options]
